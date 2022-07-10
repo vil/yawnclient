b491c817e0bbbb1a3c372f12944355dfcb102239
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-07-10 03:02:37 +0300

    test module

--- src/main/java/me/vp/yawnclient/module/modules/Test.java
+++ src/main/java/me/vp/yawnclient/module/modules/Test.java
@@ -2,12 +2,21 @@ package me.vp.yawnclient.module.modules;
 
 import me.vp.yawnclient.YawnClient;
 import me.vp.yawnclient.module.Module;
+import me.vp.yawnclient.module.setting.settings.*;
+import me.vp.yawnclient.util.JColor;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
 public class Test extends Module {
+    BooleanSetting bool = new BooleanSetting("boolean", this, true);
+    ModeSetting mode = new ModeSetting("mode", this, "mode", "mode1", "mode2");
+    KeybindSetting keybind = new KeybindSetting(GLFW.GLFW_KEY_UNKNOWN);
+    NumberSetting number = new NumberSetting("number", this, 0, 0, 100, 1);
+    ColorSetting color = new ColorSetting("color", this, new JColor(0, 0, 0));
+
     public Test() {
         super("Test","This is a test.", GLFW.GLFW_KEY_K, Category.CLIENT);
+        this.addSettings(bool, mode, keybind, number, color);
     }
 
     @Override
