30e4fe9c2d85d3dd60ab7ee1f805ae99f8029fbf
HerraVp <59320967+HerraVp@users.noreply.github.com>
2022-07-17 12:48:27 +0300

    bruh moment fix

--- src/main/java/me/vp/yawnclient/mixin/IngameHudMixin.java
+++ src/main/java/me/vp/yawnclient/mixin/IngameHudMixin.java
@@ -2,13 +2,14 @@ package me.vp.yawnclient.mixin;
 
 import me.vp.yawnclient.YawnClient;
 import me.vp.yawnclient.event.events.RenderIngameHudEvent;
+import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(IngameHudMixin.class)
+@Mixin(InGameHud.class)
 public class IngameHudMixin {
     @Inject(method = "render", at = @At("RETURN"), cancellable = true)
 	private void render(MatrixStack matrixStack, float tickDelta, CallbackInfo ci) {
